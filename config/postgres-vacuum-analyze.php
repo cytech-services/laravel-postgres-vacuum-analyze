ca357aaaba52ba4b8f89c48a3e20b59ee18a8f66
Christopher Graham <chgraham@springfieldclinic.com>
2025-02-14 10:21:28 +0200

    add configuration options for vacuum analyze command, including connections, include/exclude tables, and logging settings

--- config/postgres-vacuum-analyze.php
+++ config/postgres-vacuum-analyze.php
@@ -3,4 +3,41 @@
 // config for CytechServices/LaravelPostgresVacuumAnalyze
 return [
 
+    /**
+     * The connections to use for the vacuum analyze command.
+     */
+    'connections' => [
+        /**
+         * The connection name.
+         * By default, the connection name is the default postgres connection.
+         */
+        'pgsql' => [
+            /**
+             * The schema name.
+             * By default, the schema name is the public schema.
+             */
+            'public' => [
+                /**
+                 * A array list of tables to include in the vacuum analyze command.
+                 * If no tables are specified, all tables will be included.
+                 */
+                'include' => [
+                    // 
+                ],
+
+                /**
+                 * A array list of tables to exclude in the vacuum analyze command.
+                 */
+                'exclude' => [
+                    // 
+                ],
+            ],
+        ],
+    ],
+
+    // Enable or disable the logging of errors.
+    'log_errors' => true,
+
+    // Enable or disable verbose logging.
+    'log_verbose' => true,
 ];
